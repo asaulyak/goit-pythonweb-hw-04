49d5b76a846b9e95e09a2a114d529f9cea04a434
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2025-05-18 11:38:25 +0300

    implement file copying

---
+++ main.py
@@ -0,0 +1,54 @@
+import argparse
+import asyncio
+from aiopath import AsyncPath
+from aioshutil import copyfile
+
+
+async def read_folder(path: AsyncPath):
+    return path.iterdir()
+
+
+async def copy_file(source: AsyncPath, destination: AsyncPath):
+    await copyfile(source, destination)
+    print(f"Copied from {source} to {destination}")
+
+
+async def copy_content(source: AsyncPath, destination: AsyncPath):
+    try:
+        async for item in await read_folder(source):
+            path = AsyncPath(item)
+            current_path = AsyncPath(".") / path
+
+            is_file = await current_path.is_file()
+            if is_file:
+                suffix = path.suffix
+                extension = suffix if suffix else "(no extension)"
+
+                destination_folder_path = destination / AsyncPath(extension).name
+                destination_file_path = destination_folder_path / path.name
+
+                await destination_folder_path.mkdir(parents=True, exist_ok=True)
+                await copy_file(current_path, destination_file_path)
+            else:
+                await copy_content(current_path, destination)
+
+    except Exception as e:
+        print(f"Failed to copy files from {source} to {destination}: {e}")
+
+
+async def main():
+    parser = argparse.ArgumentParser(
+        prog="File Mapper",
+        description="Copies files to the folders based on the extension",
+    )
+
+    parser.add_argument("source")
+    parser.add_argument("output")
+
+    args = parser.parse_args()
+
+    await copy_content(AsyncPath(args.source), AsyncPath(args.output))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
